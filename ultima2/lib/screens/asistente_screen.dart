053d5c6aac8f1a832ce94e5cbd4d2a52e00f7756
BBBORYS <mellizovera@gmail.com>
2025-04-15 21:21:20 -0500

    ds

--- ultima2/lib/screens/asistente_screen.dart
+++ ultima2/lib/screens/asistente_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../providers/theme_provider.dart';
-import '../services/asistente2.dart';
 
 class AsistenteAnaScreen extends StatefulWidget {
   const AsistenteAnaScreen({Key? key}) : super(key: key);
@@ -12,6 +12,27 @@ class AsistenteAnaScreen extends StatefulWidget {
 
 class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
   bool _isActive = false;
+  late SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadState();
+  }
+
+  Future<void> _loadState() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isActive = _prefs.getBool('asistente_activo') ?? false;
+    });
+  }
+
+  Future<void> _toggleState() async {
+    setState(() {
+      _isActive = !_isActive;
+    });
+    await _prefs.setBool('asistente_activo', _isActive);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,9 +55,7 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
               colors: [
-                isDarkMode
-                    ? Colors.grey[900]!
-                    : Color.fromARGB(255, 153, 251, 174),
+                isDarkMode ? Colors.grey[900]! : Color(0xFF99FBAC),
                 isDarkMode ? Colors.grey[800]! : Color(0xFF6DD5ED),
               ],
             ),
@@ -49,9 +68,7 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              isDarkMode
-                  ? Colors.grey[900]!
-                  : Color.fromARGB(255, 125, 255, 140),
+              isDarkMode ? Colors.grey[900]! : Color(0xFF7DFF8C),
               isDarkMode ? Colors.grey[800]! : Color(0xFF6DD5ED),
             ],
           ),
@@ -91,30 +108,23 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
               ),
               SizedBox(height: 40),
               ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _isActive = !_isActive;
-                  });
-                  if (_isActive) {
-                    Asistente2.saludarUsuario();
-                  } else {
-                    Asistente2.detenerVoz();
-                  }
-                },
+                onPressed: _toggleState,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: _isActive
-                      ? Color.fromARGB(255, 87, 255, 124)
-                      : Colors.grey[700],
+                      ? Color.fromARGB(255, 120, 120, 120)
+                      : Color.fromARGB(255, 87, 255, 124),
                   padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
                 child: Text(
-                  _isActive ? 'DETENER' : 'ACTIVAR',
+                  _isActive ? 'DESACTIVAR' : 'ACTIVAR',
                   style: TextStyle(
                     fontSize: 20,
-                    color: _isActive ? Colors.black87 : Colors.white,
+                    color: _isActive
+                        ? Color.fromARGB(221, 252, 252, 252)
+                        : Color.fromARGB(255, 0, 0, 0),
                     fontWeight: FontWeight.bold,
                   ),
                 ),
