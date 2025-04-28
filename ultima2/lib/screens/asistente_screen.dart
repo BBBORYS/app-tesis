550158eae202783cd6ca35b2479e4336432ec77e
BBBORYS <mellizovera@gmail.com>
2025-04-28 17:23:51 -0500

    ul

--- ultima2/lib/screens/asistente_screen.dart
+++ ultima2/lib/screens/asistente_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../providers/theme_provider.dart';
 
 class AsistenteAnaScreen extends StatefulWidget {
@@ -11,28 +10,7 @@ class AsistenteAnaScreen extends StatefulWidget {
 }
 
 class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
-  bool _isActive = false;
-  late SharedPreferences _prefs;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadState();
-  }
-
-  Future<void> _loadState() async {
-    _prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _isActive = _prefs.getBool('asistente_activo') ?? false;
-    });
-  }
-
-  Future<void> _toggleState() async {
-    setState(() {
-      _isActive = !_isActive;
-    });
-    await _prefs.setBool('asistente_activo', _isActive);
-  }
+  bool _isOn = false;
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +33,9 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
               colors: [
-                isDarkMode ? Colors.grey[900]! : Color(0xFF99FBAC),
+                isDarkMode
+                    ? Colors.grey[900]!
+                    : Color.fromARGB(255, 153, 251, 174),
                 isDarkMode ? Colors.grey[800]! : Color(0xFF6DD5ED),
               ],
             ),
@@ -68,7 +48,9 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              isDarkMode ? Colors.grey[900]! : Color(0xFF7DFF8C),
+              isDarkMode
+                  ? Colors.grey[900]!
+                  : Color.fromARGB(255, 125, 255, 140),
               isDarkMode ? Colors.grey[800]! : Color(0xFF6DD5ED),
             ],
           ),
@@ -83,7 +65,7 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   border: Border.all(
-                    color: _isActive ? Colors.white : Colors.grey[300]!,
+                    color: _isOn ? Colors.green : Colors.grey[300]!,
                     width: 3,
                   ),
                   boxShadow: [
@@ -106,26 +88,92 @@ class _AsistenteAnaScreenState extends State<AsistenteAnaScreen> {
                   ),
                 ),
               ),
-              SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: _toggleState,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: _isActive
-                      ? Color.fromARGB(255, 120, 120, 120)
-                      : Color.fromARGB(255, 87, 255, 124),
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
+              SizedBox(height: 20),
+              Text(
+                _isOn ? 'Asistente activado' : 'Asistente desactivado',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: isDarkMode ? Colors.white : Colors.black,
                 ),
-                child: Text(
-                  _isActive ? 'DESACTIVAR' : 'ACTIVAR',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: _isActive
-                        ? Color.fromARGB(221, 252, 252, 252)
-                        : Color.fromARGB(255, 0, 0, 0),
-                    fontWeight: FontWeight.bold,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 40),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isOn = !_isOn;
+                  });
+                },
+                child: Container(
+                  width: 120,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: _isOn
+                        ? Color.fromARGB(255, 1, 248, 54).withOpacity(0.3)
+                        : Colors.grey[400]!.withOpacity(0.3),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 6,
+                          offset: Offset(0, 3))
+                    ],
+                  ),
+                  child: Stack(
+                    children: [
+                      if (!_isOn)
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 20),
+                            child: Text(
+                              'OFF',
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      if (_isOn)
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Text(
+                              'ON',
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      AnimatedAlign(
+                        duration: Duration(milliseconds: 200),
+                        alignment: _isOn
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          margin: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 87, 220, 247),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                blurRadius: 4,
+                                spreadRadius: 1,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
